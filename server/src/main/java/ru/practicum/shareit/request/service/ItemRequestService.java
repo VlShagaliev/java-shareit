94fbead8ea45695c72163701f1530de902a5e3d6
VlShagaliev <vl.shagaliev@yandex.ru>
2025-11-15 14:31:32 +0500

    Исправить замечания по комеентарию ревьювера без добавления Мок тестирования. Исправить ошибку при прохождении тестирования github.

--- server/src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ server/src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface ItemRequestService {
     ItemRequestDto createItemRequest(ItemRequestDto itemRequestDto, Long userId);
+
     List<ItemRequestDto> getItemRequestByUserId(Long userId);
+
     ItemRequestDto getItemRequestByItemRequestId(Long itemRequestId);
+
     List<ItemRequestDto> getItemRequestsByOtherUsers(Long userId);
 }
