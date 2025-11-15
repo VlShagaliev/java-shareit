b344f47fe87c189719421e1e7c8589ef4c5b8579
VlShagaliev <vl.shagaliev@yandex.ru>
2025-11-15 18:02:25 +0500

    Добавить МОК тестирование, интеграционные тесты.

--- gateway/src/test/java/ItemControllerTest.java
+++ gateway/src/test/java/ItemControllerTest.java
@@ -15,7 +15,6 @@ import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.UserDto;
 
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.any;
